c0684462ba82063aaddeeeab37ec626d8ec6609a
2siwon <lee89640245@gmail.com>
2017-11-18 20:35:26 +0900

    Images 모델 생성 및 추가, admin에 모델 등록

--- nanum/posts/admin.py
+++ nanum/posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import answer, question, images
 
-# Register your models here.
+admin.site.register(answer)
+admin.site.register(question)
+admin.site.register(images)
