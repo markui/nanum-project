ecc0ed0db4c56168eaebb1b0a894dc70fa677131
John Sangwon Suh <johnsuh94@gmail.com>
2017-11-22 14:44:37 +0900

    posts/comment 모델 생성, topics 모델 생성

--- nanum/topics/models.py
+++ nanum/topics/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Topic(models.Model):
-    pass
+    """
+    토픽 모델
+    Question과 Answer에 Foreinkey로 연결이 되어 있음
+
+    """
+    name = models.TextField(
+        max_length=200,
+    )
+
