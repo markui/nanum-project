083ff2e1bbc06fe7bb79c1870988a71d23cc63fa
John Sangwon Suh <johnsuh94@gmail.com>
2017-11-23 12:22:25 +0900

    토픽 모델에 필드, 주석 추가

--- nanum/topics/models.py
+++ nanum/topics/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+
+
 # Create your models here.
 class Topic(models.Model):
     """
     토픽 모델
-    Question과 Answer에 Foreinkey로 연결이 되어 있음
-
+    Question과 Answer에 ManyToMany로 연결
+    
     """
     name = models.TextField(
         max_length=200,
+    )
+    image = models.ImageField(
+        upload_to='topic',
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
     )
