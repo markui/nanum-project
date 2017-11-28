6e52058bf221d4f401b7814b0e1681327eae48b3
John Sangwon Suh <johnsuh94@gmail.com>
2017-11-29 00:52:48 +0900

    UPDATE: AnswerListCreateView - Create method 주석 추가, permission IsAuthenticate로 변경 ADD: AnswerMainFeed List - get_queryset method 에 기본적인 필터링 추가 ADD: AnswerListCreateApisTest - authenticated 되지 않았을 때 기본 AnswerListViewㅔ 대한 test 추가 UPDATE: Topic API class명 변경, url 수정

--- nanum/posts/apis/answer.py
+++ nanum/posts/apis/answer.py
@@ -1,9 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend, filters
 from rest_framework import generics, permissions
-from rest_framework.response import Response
 
-from ..serializers.answer import AnswerSerializer, AnswerFeedSerializer
 from ..models import Answer
+from ..serializers.answer import AnswerSerializer, AnswerFeedSerializer
 
 __all__ = (
     'AnswerListCreateView',
@@ -20,28 +19,71 @@ class AnswerListCreateView(generics.ListCreateAPIView):
     queryset = Answer
     serializer_class = AnswerSerializer
     permission_classes = (
-        permissions.IsAuthenticatedOrReadOnly,
+        permissions.IsAuthenticated,
     )
 
     def get_queryset(self):
         user = self.request.user
         return user.answer_set.all()
 
+    def create(self, request, *args, **kwargs):
+        """
+        Request에서 quill.js json 파일을 content로 받음
+        사진 관련 파일에 대해 bytecode parsing -> save as jpg to S3 -> convert bytecode to S3 link
+        변환된 데이터를 가지고 Answer 객체 생성
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return super().create(request, *args, **kwargs)
+
+
 class AnswerMainFeedListView(generics.ListAPIView):
-    queryset = Answer
+    """
+    유저를 위한 주 답변 Feed
+    1. Topic, Follower를 기반으로 개인화된 피드 생성
+    2. +추후 Like 정보 추가
+    3. +추후 CF Filtering / Content-based Filtering 적용
+    """
     serializer_class = AnswerFeedSerializer
 
-    def list(self, request, *args, **kwargs):
+    def get_queryset(self):
+        """
+        Topic, Follower 기반 filter된 queryset 반환
+        :return:
+        """
+        user = self.request.user
 
-        pass
+        # Get all Topics that user is following
+        answer_topic_interest = list(user.topic_interests.values_list(flat=True))
+        answer_topic_expertise = list(user.topic_expertise.values_list(flat=True))
+
+        # Combine both
+        answer_topic = answer_topic_interest.extend(answer_topic_expertise)
+
+        # Get Follower's Answers
+        following_users = user.following.values_list(flat=True)
+
+        # Filter Answer, order by the most recently modified post
+        queryset = Answer.objects.filter(topic__in=answer_topic) \
+            .filter(user__in=following_users) \
+            .order_by('modified_at')
+
+        return queryset
+
 
 class AnswerBookmarkFeedListView(generics.ListAPIView):
+    """
+    유저가 북마크한 답변 필터 목록
+    """
     queryset = Answer
     serializer_class = AnswerFeedSerializer
 
     def get_queryset(self):
         user = self.request.user
         return user.bookmarked_questions.all()
+
 
 class AnswerFilterFeedListView(generics.ListAPIView):
     """
@@ -52,4 +94,3 @@ class AnswerFilterFeedListView(generics.ListAPIView):
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     filter_fields = ('topic')
     ordering_fields = ('modified_at')
-
