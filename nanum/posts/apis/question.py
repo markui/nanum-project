84ca025689c1d2ff57e72eca3608b88d551512d9
2siwon <lee89640245@gmail.com>
2017-12-04 07:41:48 +0900

    내가 북마크한 질문 리스트 뷰 작성

--- nanum/posts/apis/question.py
+++ nanum/posts/apis/question.py
@@ -11,8 +11,8 @@ __all__ = (
 
 #########################################
 # 1. 전문분야 질문 리스트(질문 읽기 페이지 메인) [X]
-# 2. 북마크한 질문 리스트 []
-# 3. 최신 질문 리스트 []
+# 2. 북마크한 질문 리스트 [X]
+# 3. 최신 질문 리스트 [X]
 # 4. 나에게 요청된 질문 리스트 []
 # 5. 답변 중인 질문 리스트 []
 #########################################
@@ -31,7 +31,7 @@ class QuestionListCreateView(generics.ListCreateAPIView):
         # 사용자가 선택한 전문분야 토픽
         topics = self.request.user.topic_expertise.all()
         # query-set(자기가 쓴 글 제외, 선택한 전문분야의 글 모두 취합)
-        for index, topic in enumerate(topics):
+        for topic in topics:
             query_set = exclude_user & Question.objects.filter(topics=topic.id) | query_set
 
         return query_set
@@ -47,3 +47,20 @@ class QuestionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
     )
+
+
+# 내가 북마크 한 질문리스트
+class BookMarkedQuestionListView(generics.ListAPIView):
+    serializer_class = QuestionSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+    )
+
+    def get_queryset(self):
+        query_set = Question.objects.none()
+        bookmarked_questions = self.request.user.bookmarked_questions.all()
+
+        for question in bookmarked_questions:
+            query_set = Question.objects.get(id=question.id) | query_set
+
+        return query_set
