b8a6e88ba4ce1b1fad2468c6ff835c2f6b9e5bf0
John Sangwon Suh <johnsuh94@gmail.com>
2017-12-09 13:56:45 +0900

    ADD: Topic 모델에 expertise, interest follower 수에 대한 increment 추가

--- nanum/topics/models.py
+++ nanum/topics/models.py
@@ -8,25 +8,17 @@ class Topic(models.Model):
     토픽 모델
     Question과 Answer에 ManyToMany로 연결
     """
-    creator = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    name = models.TextField(
-        max_length=200,
-    )
-    description = models.TextField(
-        max_length=300,
-        blank=True,
-    )
-    image = models.ImageField(
-        upload_to='topic',
-        blank=True,
-    )
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    name = models.TextField(max_length=100, unique=True, blank=False, null=False)
+    description = models.TextField(max_length=300, blank=True)
+    image = models.ImageField(upload_to='topic', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    answer_count = models.IntegerField(null=False, default=0)
+    question_count = models.IntegerField(null=False, default=0)
+    expert_count = models.IntegerField(null=False, default=0)
+    interest_count = models.IntegerField(null=False, default=0)
+
 
     def __str__(self):
-        return f'{self.name}\ncreator:{self.creator}'
+        return f'{self.name}, creator:{self.creator}'
