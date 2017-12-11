f0f4a299f060b68e3dee5206eb53ded4d4a996d6
2siwon <lee89640245@gmail.com>
2017-12-11 22:17:12 +0900

    QuestionListCreateViewTest url reslove 테스트 추가

--- nanum/posts/tests/test_api/test_question.py
+++ nanum/posts/tests/test_api/test_question.py
@@ -13,17 +13,25 @@ User = get_user_model()
 
 
 class QuestionListCreateViewTest(APILiveServerTestCase):
-    URL_API_QUESTION_CREATE_NAME = 'posts:question:question-create'
-    URL_API_QUESTION_CREATE = '/posts/question/'
+    URL_API_QUESTION_CREATE_NAME = 'post:question:question-list'
+    URL_API_QUESTION_CREATE = '/post/question/'
     VIEW_CLASS = QuestionListCreateView
 
-    # 원하는 URL과 실제로 만들어지는 URL 같은지 검사
+    # URL name으로 원하는 URL과 실제로 만들어지는 URL 같은지 검사
     def test_question_create_url_name_reverse(self):
         url = reverse(self.URL_API_QUESTION_CREATE_NAME)
         print(f'reverse test : {url}')
         self.assertEqual(url, self.URL_API_QUESTION_CREATE)
 
+    # URL이 실제 URL name을 참조하고 있는지 검사
+    def test_question_create_url_name_resolve(self):
+        resolve_match = resolve(self.URL_API_QUESTION_CREATE)
+        print(f'resolve test(url name) : {resolve_match.namespace + ":" + resolve_match.url_name}')
+        self.assertEqual(resolve_match.namespace + ":" + resolve_match.url_name, self.URL_API_QUESTION_CREATE_NAME)
+
     # 같은 view의 class인지 검사
+    # .func 는 임시함수, .as_view() 또한 함수이다. 참조하는 주소 값이 다르므로 .func.view_class 로 비교
+    # self.VIEW_CLASS == self.VIEW_CLASS.as_view().view_class : True
     def test_question_create_url_resolve_view_class(self):
         resolve_match = resolve(self.URL_API_QUESTION_CREATE)
         print(f'view class test : {resolve_match.func.view_class}')
