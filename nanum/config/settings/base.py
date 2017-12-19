cefa26fce5cc1fccf41ba6eb476e568d35628a7f
Mark Kim <viking617617@gmail.com>
2017-12-19 19:57:03 +0900

    MODIFY: thumbnail_image가 없는 경우, default 이미지 url을 사용하도록 ImageField Customizing

--- nanum/config/settings/base.py
+++ nanum/config/settings/base.py
@@ -161,6 +161,9 @@ RAVEN_CONFIG = {
     # release based on the git info.
     'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
 }
+# Getstream
+STREAM_API_KEY = config_secret_common['getstream']['api_key']
+STREAM_API_SECRET = config_secret_common['getstream']['api_secret']
 
 # Celery
 # CELERY_BROKER_URL = 'amqp://localhost'
